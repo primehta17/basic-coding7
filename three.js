a975d358d2bab7692f23e9d77a6f36fbfe86bbd6
Priyanka Mehta <primehta17@gmail.com>
2025-04-01 11:17:10 +0530

    ascii value

---
+++ three.js
@@ -0,0 +1,8 @@
+//JavaScript Program to Find ASCII Value of Character
+
+let char='S';let code=66;
+
+console.log("The ASCII value is:", char.charAt());
+console.log("The ASCII value is:", char.charCodeAt());
+console.log("The ASCII value is:", char.codePointAt());
+console.log(String.fromCharCode(code));
